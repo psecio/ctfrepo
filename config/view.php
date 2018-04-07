6eeea7e2e1e2af260cdb32e538ef58be917dc08a
Chris Cornutt <enygma@phpdeveloper.org>
2018-04-07 07:56:37 -0500

    adding logic to the view to sort the challenges by #

--- config/view.php
+++ config/view.php
@@ -15,11 +15,17 @@ class TemplateView extends \Slim\Views\Twig
                 if (preg_match('/challenge([0-9]+)/', $filename, $match)) {
                     $data['challenges'][] = [
                         'link' => $filename,
-                        'name' => 'Challenge '.$match[1]
+                        'name' => 'Challenge '.$match[1],
+                        'id' => $match[1]
                     ];
                 }
             }
         }
+
+        // Sort them
+        usort($data['challenges'], function($value1, $value2) {
+            return $value1['id'] > $value2['id'];
+        });
 
         return $this->render($response, $view, $data);
     }
