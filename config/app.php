9c29cc466c58873f886691570cc3673811c34de1
Chris Cornutt <enygma@phpdeveloper.org>
2018-04-08 05:45:23 -0500

    adding the db connection to the container

--- config/app.php
+++ config/app.php
@@ -3,6 +3,9 @@
 require_once __DIR__.'/../vendor/autoload.php';
 require_once __DIR__.'/view.php';
 
+$dotenv = new Dotenv\Dotenv(__DIR__.'/../');
+$dotenv->load();
+
 $app = new Slim\App();
 $container = $app->getContainer();
 
@@ -11,4 +14,9 @@ $container['view'] = function() use ($container) {
     $view['container'] = $container;
 
     return $view;
+};
+
+$container['db'] = function() {
+    $dsn = 'mysql:dbname='.$_ENV['MYSQL_DATABASE'].';host='.$_ENV['MYSQL_HOST'];
+    return new \PDO($dsn, $_ENV['MYSQL_USER'], $_ENV['MYSQL_PASSWORD']);
 };
