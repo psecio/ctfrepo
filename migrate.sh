370b8d052ef0b02ccfea2243053b2881a6ab0756
Chris Cornutt <enygma@phpdeveloper.org>
2018-04-10 14:44:03 -0500

    updating Docker config and migration path

--- migrate.sh
+++ migrate.sh
@@ -2,5 +2,6 @@
 
 echo "Running migrations and seeding data"
 
-/var/www/html/vendor/bin/phinx migrate
-/var/www/html/vendor/bin/phinx seed:run
+cd /var/www/html
+./vendor/bin/phinx migrate
+./vendor/bin/phinx seed:run
