2b679b48b061b80a482a467ae60e4e44f110b8f4
Chris Cornutt <enygma@phpdeveloper.org>
2018-04-12 05:53:01 -0500

    adding the default logout route

--- public/index.php
+++ public/index.php
@@ -7,4 +7,9 @@ $app->get('/', function() {
     return $this->view->write('/index/index.php', $data);
 });
 
+$app->get('/logout', function() {
+    $this->session->set('user', null);
+    return $this->response->withRedirect('/');
+});
+
 $app->run();
